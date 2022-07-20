a676dd8046b220daf15f451c4327b5de31eb1ddc
govldbstj <chldbstj0512@gmail.com>
2022-07-20 15:41:20 +0900

    first

---
+++ src/test/java/com/ajou/prcoding/myweb/MyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajou.prcoding.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
